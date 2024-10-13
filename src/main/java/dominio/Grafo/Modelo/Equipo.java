3a9850c2a1379b0fb22de3753237d0906e5bc328
rodriguezfacundo <facundorodriguez4141@gmail.com>
2024-10-13 19:16:28 -0300

    Se realiza la correcion del metodo agregarJugadorAEquipo validacion 6. Se realiza el requerimiento 10 (listarJugadoresDeEquipo). Se ponen comentarios a revisar luego en los metodos listarEquiposDescendente, listarJugadoresPorCategoria, listarJugadoresAscendente, ya que me parece que no estamos cumpliendo con el orden de busqueda que piden

--- src/main/java/dominio/Grafo/Modelo/Equipo.java
+++ src/main/java/dominio/Grafo/Modelo/Equipo.java
@@ -5,12 +5,12 @@ import dominio.ABB.ABB;
 public class Equipo implements Comparable<Equipo> {
     private String nombre;
     private String mangaer;
-    private ABB arbolJugadores;
+    private ABB jugadores;
 
     public Equipo(String nombre, String mangaer) {
         this.nombre = nombre;
         this.mangaer = mangaer;
-        arbolJugadores = new ABB();
+        jugadores = new ABB();
     }
 
     public String getNombre() {
@@ -29,29 +29,29 @@ public class Equipo implements Comparable<Equipo> {
         this.mangaer = mangaer;
     }
 
-    public ABB getArbolJugadores() {
-        return arbolJugadores;
+    public ABB getJugadores() {
+        return jugadores;
     }
 
-    public void setArbolJugadores(ABB arbolJugadores) {
-        this.arbolJugadores = arbolJugadores;
+    public void setJugadores(ABB jugadores) {
+        this.jugadores = jugadores;
     }
 
     protected int getCantidadJugadores(){
-        return this.arbolJugadores.cantNodos();
+        return this.jugadores.cantNodos();
     }
 
     public boolean excedeMaximoJugadores(){
         return getCantidadJugadores() > 5;
     }
-
     @Override
     public int compareTo(Equipo o) {
         return this.nombre.compareTo(o.nombre);
     }
 
     public void agregarJugador(Jugador jugador) {
-        this.arbolJugadores.agregarDato(jugador);
+        this.jugadores.agregarDato(jugador);
+        jugador.registradoEnEquipo();
     }
 
     public String listarJugadores() {
